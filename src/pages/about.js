93c07e72d82d72584cdbfca41890bc2f5fd4bcd7
szekelybalint0101 <szekelybalint0101@gmail.com>
2023-04-16 18:15:19 +0200

    project init

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+return (
+	<div>
+        <div className="container">
+        <h1>
+		How to use?
+	</h1>
+    <p>
+        Upload a test .txt file. The program will list all words and translate them. 
+    </p>
+    <p>You can save the result into a txt file.</p>
+        </div>
+	</div>
+);
+};
+
+export default About;
